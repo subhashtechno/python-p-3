452aac7fb8abe32a71c895d83d6b07a2408112ce
subhashtechno <107833238+subhashtechno@users.noreply.github.com>
2022-07-19 19:42:34 +0530

    first commit

---
+++ desktopnotifier.py
@@ -0,0 +1,11 @@
+from player import notification
+import time
+
+if __name__ == ' __main__':
+       while True:
+        notification.notify(
+            title="*** Take Rest ***",
+            message="Rest is vital for better mental health, inc
+            app_icon="D:/notification/try.ico",
+            timeout=5)
+            time.sleep(20)
